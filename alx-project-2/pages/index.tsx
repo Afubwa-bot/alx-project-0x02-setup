920bb3b4532e12438f1a5d36712377270ffaf3c0
Afubwa. <emmanelafubwa@gmail.com>
2025-07-29 18:59:04 +0300

    setting up a Next.js Project

---
+++ alx-project-2/pages/index.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+const Homepage: React.FC = () => {
+  return (
+    <div>
+    <h1>Welcome to My Next.js Project</h1>
+    </div>
+  );
+};
+export default Homepage;
